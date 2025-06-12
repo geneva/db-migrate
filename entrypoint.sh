d12a980ffea98c6069dd2829c158fd6e7fe0c955
matt handler <matt.handler@gmail.com>
2025-06-12 12:17:20 -0400

    add new command

--- entrypoint.sh
+++ entrypoint.sh
@@ -76,6 +76,13 @@ case "$1" in
   psql)
     psql --dbname="$DB_NAME" --host="$DB_HOST" --username="$DB_USER"
     ;;
+  command)
+    if [ -z "$2" ]; then
+      print_usage
+      exit 1
+    fi
+    psql --dbname="$DB_NAME" --host="$DB_HOST" --username="$DB_USER" --command="$2"
+    ;;
   *)
     print_usage
     exit 1
